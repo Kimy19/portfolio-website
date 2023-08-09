31014fcfaae270aa21dd0ecabbe5a442eead273c
Kim Y.C <yckim19@naver.com>
2023-08-09 13:22:48 +0900

    배포 전

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // 이미지 가져오는 도메인 정보 등록
+  images:{
+    domains:[
+      'www.notion.so',
+      'images.unsplash.com',
+      'user-images.githubusercontent.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
